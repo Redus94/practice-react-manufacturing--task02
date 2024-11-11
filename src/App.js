6710181e818f3948272aeea887778b4aa6d826b8
Paweł Suder <pawel.suder94@gmail.com>
2024-11-11 19:03:16 +0100

    add text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React faster
+          Learn React faster 24
         </a>
       </header>
     </div>
